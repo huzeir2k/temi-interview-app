9dcd7bfa8f8a5b9359b9369da3831a81d4baab14
huzeir2k <huzeir2k@gmail.com>
2021-10-03 14:28:18 -0400

    * added several methods to mainactivity * designed the initial layout of the application
    more things need to be finished and navigation/map methods need to be implemented as soon as possible

--- app/src/main/java/com/example/temi_interview_app_project/MainActivity.java
+++ app/src/main/java/com/example/temi_interview_app_project/MainActivity.java
@@ -1,14 +1,128 @@
 package com.example.temi_interview_app_project;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.robotemi.sdk.NlpResult;
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.TtsRequest;
+import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage;
+import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener;
+import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
+import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
+import com.robotemi.sdk.listeners.OnRobotReadyListener;
+
+import java.util.List;
+
+
+/*
+    APPLICATION NAME: Temi Interview App
+    DEVELOPERS RESPONSIBLE: Huzeir Kurpejovic, Dylan Shawol
+    APPLICATION'S FUNCTION: Allows the robot Temi to move to different rooms and
+ */
+
+
+public class MainActivity extends AppCompatActivity implements
+        Robot.NlpListener,
+        OnRobotReadyListener,
+        Robot.ConversationViewAttachesListener,
+        Robot.WakeupWordListener,
+        Robot.ActivityStreamPublishListener,
+        Robot.TtsListener,
+        OnBeWithMeStatusChangedListener,
+        OnGoToLocationStatusChangedListener,
+        OnLocationsUpdatedListener {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    //ON START METHODS
+
+    protected void onStart() {
+        super.onStart();
+        Robot.getInstance().addOnRobotReadyListener(this);
+        Robot.getInstance().addNlpListener(this);
+        Robot.getInstance().addOnBeWithMeStatusChangedListener(this);
+        Robot.getInstance().addOnGoToLocationStatusChangedListener(this);
+        Robot.getInstance().addConversationViewAttachesListenerListener(this);
+        Robot.getInstance().addWakeupWordListener(this);
+        Robot.getInstance().addTtsListener(this);
+        Robot.getInstance().addOnLocationsUpdatedListener(this);
+    }
+
+    //ON STOP METHODS
+
+    protected void onStop() {
+        super.onStop();
+        Robot.getInstance().removeOnRobotReadyListener(this);
+        Robot.getInstance().removeNlpListener(this);
+        Robot.getInstance().removeOnBeWithMeStatusChangedListener(this);
+        Robot.getInstance().removeOnGoToLocationStatusChangedListener(this);
+        Robot.getInstance().removeConversationViewAttachesListenerListener(this);
+        Robot.getInstance().removeWakeupWordListener(this);
+        Robot.getInstance().removeTtsListener(this);
+        Robot.getInstance().removeOnLocationsUpdateListener(this);
+    }
+
+
+    @Override
+    public void onPublish(@NonNull ActivityStreamPublishMessage activityStreamPublishMessage) {
+
+    }
+
+    @Override
+    public void onConversationAttaches(boolean b) {
+
+    }
+
+    @Override
+    public void onNlpCompleted(@NonNull NlpResult nlpResult) {
+
+    }
+
+    @Override
+    public void onTtsStatusChanged(@NonNull TtsRequest ttsRequest) {
+
+    }
+
+    @Override
+    public void onWakeupWord(@NonNull String s, int i) {
+
+    }
+
+    @Override
+    public void onLocationsUpdated(@NonNull List<String> list) {
+
+    }
+
+    @Override
+    public void onBeWithMeStatusChanged(@NonNull String s) {
+
+    }
+
+    @Override
+    public void onGoToLocationStatusChanged(@NonNull String s, @NonNull String s1, int i, @NonNull String s2) {
+
+    }
+
+    @Override
+    public void onRobotReady(boolean isReady) {
+        if (isReady) {
+            try {
+                //TODO: get rid of this error
+                final ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+                robot.onStart(activityInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
     }
 }
